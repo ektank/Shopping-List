3f5d25cdf8257ba0975ce9dffa136da0a947dda3
Emily K. Tankersley <emilytankersley@gmail.com>
2014-03-04 21:31:33 -0800

    Finished?

---
+++ shop.js
@@ -0,0 +1,41 @@
+function addListItem() {
+    
+     var write = $('#newItem').val();
+     var list = $('#itemList');
+     var item = $('<li><button class="checkbox">&#x2713;</button><span class="list">' + write + '</span><button class="delete">X</button></li>');
+     var newItem = $("#newItem");
+
+     if (write.length === 0  || write.length > 40) {
+        return false;
+    }
+    
+    list.append(item);
+    $(newItem).val('');
+}
+
+function deleteItem() {
+
+    $(this).parent().remove();
+}
+
+function dimItem() {
+
+    $(this).parent().toggleClass('dim');
+}
+
+$(function() {
+
+    var add = $('#addItem');
+    var newItem = $('#newItem');
+    var list = $('#itemList');
+    
+    add.on('click', addListItem);
+    list.on('click', '.checkbox', dimItem);
+    list.on('click', '.delete', deleteItem);
+    newItem.on('keypress', function (e) {
+        if (e.which == 13) {
+            addListItem();
+        }
+    });
+});
+
